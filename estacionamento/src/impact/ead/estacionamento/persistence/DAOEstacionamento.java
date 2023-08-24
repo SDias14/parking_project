c13db3181ed5b67c5ffb2522603454309327343b
samuel <samueld2014@gmail.com>
2023-08-24 15:49:25 -0300

    dois nao comitados

--- estacionamento/src/impact/ead/estacionamento/persistence/DAOEstacionamento.java
+++ estacionamento/src/impact/ead/estacionamento/persistence/DAOEstacionamento.java
@@ -1,5 +1,29 @@
 package impact.ead.estacionamento.persistence;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import impact.ead.estacionamento.negocio.Movimentacao;
+
 public class DAOEstacionamento {
+	
+	public void criar(Movimentacao movimentacao) {
+		//TODO implementar
+	}
+	
+	public void atualizar(Movimentacao movimentacao) {
+		//TODO implementar
+		
+	}
+	
+	public Movimentacao buscarMovimentacaoAberta(String placa) {
+		//TODO implementar
+		return null;
+	}
+	
+	public List<Movimentacao> consultarMovimentacoes(LocalDateTime data){
+		//TODO implementar
+		return null;
+	}
 
 }
--- estacionamento/src/impact/ead/estacionamento/utilitario/EstacionamentoUtil.java
+++ estacionamento/src/impact/ead/estacionamento/utilitario/EstacionamentoUtil.java
@@ -1,5 +1,38 @@
 package impact.ead.estacionamento.utilitario;
 
+import impact.ead.estacionamento.negocio.Movimentacao;
+
+
+/**
+ * Representa um classe de apoio as demais do sistema
+ * 
+ * @author sdias
+ * 
+ * 
+ */
 public class EstacionamentoUtil {
+	
+	/**
+	 * Valida a placa com o padrao nacional : LLL-NNNN
+	 * L = LETRA
+	 * N = NUMERO
+	 * @param placa = Placa do veiculo 
+	 * @return true se atender o padrao e false se nao. 
+	 */
+	public boolean validarPadraoPlaca(String placa) {
+		//TODO implementar
+		return false;
+	}
+	/**
+	 * Realiza o calculo do valor da estada do veiculo baseado no tarifario
+	 * e na hora de entrada e saida do veiculo
+	 * 
+	 * altera a propria instancia do parametro. 
+	 * 
+	 * @param movimentacao Instancia da movimentacao 
+	 */
+	public void calcularPagamento(Movimentacao movimentacao) {
+		//TODO implementar
+	}
 
 }
